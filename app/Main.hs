3a1049c7d5d3bd7ee7d84bdc8a9abf63bd658227
Mizunashi Mana <mizunashi-mana@noreply.git>
2017-06-18 11:56:45 +0900

    Add some code

--- app/Main.hs
+++ app/Main.hs
@@ -6,3 +6,6 @@ main :: IO ()
 main = do
   putStr "12 + 6 = "
   print $ simpleAdd 12 6
+
+  putStr "12 - 6 = "
+  print $ simpleSub 12 6
--- src/Lib.hs
+++ src/Lib.hs
@@ -11,3 +11,9 @@ simpleAdd n 0 = n
 simpleAdd n m
   | m < 0 = simpleAdd (dec n) $ inc m
   | m > 0 = simpleAdd (inc n) $ dec m
+
+simpleSub :: Int -> Int -> Int
+simpleSub n 0 = n
+simpleSub n m
+  | m < 0 = simpleSub (inc n) $ inc m
+  | m > 0 = simpleSub (dec n) $ dec m
